afe6bf8faae2155a59b275682c4286dadb1d372b
arjunTltech <arjunntltechnologies@gmail.com>
2025-03-21 17:16:38 +0530

    feat: added middlewares

--- src/routes/admin/socialRoute.js
+++ src/routes/admin/socialRoute.js
@@ -2,14 +2,17 @@ import  express from 'express'
 import Repositorys from '../../repositories/repositorys.js'
 import SocialService from '../../services/socialService.js'
 import SocialController from '../../controllers/socialController.js'
+import verifyJwtToken from '../../middlewares/verifyJwtToken.js'
 
 
 const router =express.Router()
 const socialController = new SocialController(new SocialService(new Repositorys))
+
 router.post('/create-social',(req,res)=>socialController.createSocial(req,res))
-router.get('/get-social',(req,res)=>socialController.getAllSocials(req,res))
-router.put('/update-social/:id',(req,res)=>socialController.updateSocial(req,res))
 router.delete('/delete-social/:id',(req,res)=>socialController.deleteSocial(req,res))
-router.get('/get-social/:id',(req,res)=>socialController.getSocialById(req,res))
+
+router.get('/get-social',verifyJwtToken,(req,res)=>socialController.getAllSocials(req,res))
+router.put('/update-social/:id',verifyJwtToken,(req,res)=>socialController.updateSocial(req,res))
+router.get('/get-social/:id',verifyJwtToken,(req,res)=>socialController.getSocialById(req,res))
 
 export default router
